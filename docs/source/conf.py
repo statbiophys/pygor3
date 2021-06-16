00b330a108082841333e171a99cb095d652e0214
Carlos Olivares <alfaceor>
2021-06-16 15:44:14 +0200

    Readthedocs: myst_parser removed

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,6 @@ author = 'Carlos Olivares'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-        'myst_parser',
         'sphinx.ext.autodoc',
         'sphinx.ext.coverage', 'sphinx.ext.napoleon',
         'sphinx_markdown_tables','nbsphinx'
